f61022db6570efcc3bd172a952a00923dfb3307b
Arthur trindade <102676263+trindadeee@users.noreply.github.com>
2023-11-13 19:57:55 -0300

    Update ForgotPasswor.tsx

--- src/pages/forgotPassword/ForgotPasswor.tsx
+++ src/pages/forgotPassword/ForgotPasswor.tsx
@@ -1,10 +1,1 @@
-import React from 'react'
-import {Text} from 'react-native';
 
-const ForgotPassword = () => {
-  return (
-    <Text>ForgotPassword</Text>
-  );
-};
-
-export default ForgotPassword
