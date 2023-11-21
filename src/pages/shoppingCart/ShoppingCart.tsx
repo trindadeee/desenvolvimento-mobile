cb8af04177e0fd5587f0363d63b76256a5484628
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-11-20 23:36:53 -0300

    Update ShoppingCart.tsx

--- src/pages/shoppingCart/ShoppingCart.tsx
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react';
+import { ImageBackground, Text, View, Image } from 'react-native';
 import { Button } from 'react-native-elements';
+import styles from '../login/LoginStyle';
 
 const ShoppingCart = ({ route, navigation }: any) => {
   const { shoppingCart } = route.params;
 
   const removeItem = (index: number) => {
-  
     const updatedCart = [...shoppingCart];
     const itemToRemove = updatedCart[index];
-  
+
     if (itemToRemove.quantity > 1) {
       itemToRemove.quantity -= 1;
     } else {
@@ -18,21 +18,22 @@ const ShoppingCart = ({ route, navigation }: any) => {
     navigation.setParams({ shoppingCart: updatedCart });
   };
 
-
   return (
-    <>
-      {shoppingCart.map((prod: any, i: number) => (
-        <View style={{ display:'flex', alignItems: 'center' , marginTop: 10, marginBottom: 10,  }} key={i}>
-          <Text>{prod.name}</Text>
-          <Text>quantidade: {prod.quantity}</Text>
-          <View style={{margin: 10, width: '95%'}} >
-            <Button title="Remover Item"onPress={() => removeItem(i)} />
+    <ImageBackground style={styles.imageBack} source={require('pharmacy-mobile/assets/farmBackground.jpg')}>
+      <>
+        {shoppingCart.map((prod: any, i: number) => (
+          <View key={i} style={{ display: 'flex', alignItems: 'center', marginTop: 10, marginBottom: 10 }}>
+            <Image source={{ uri: prod.image }} style={{ width: 100, height: 100 }} />
+            <Text>{prod.name}</Text>
+            <Text>Quantidade: {prod.quantity}</Text>
+            <View style={{ margin: 10, width: '95%' }}>
+              <Button title="Remover Item" onPress={() => removeItem(i)} />
+            </View>
           </View>
-          
-        </View>
-      ))}
-    </>
+        ))}
+      </>
+    </ImageBackground>
   );
-}
+};
 
 export default ShoppingCart;
