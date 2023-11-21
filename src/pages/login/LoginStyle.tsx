14311395bda853f33b14fc158c939c432a2d9ec5
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-11-20 21:04:03 -0300

    Update LoginStyle.tsx

--- src/pages/login/LoginStyle.tsx
+++ src/pages/login/LoginStyle.tsx
@@ -1,41 +1,45 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+  imageBack: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
     container: {
       flex: 1,
-      backgroundColor: '#fff',
       justifyContent: 'center',
-      padding: 40
-      
+      padding: 40,
     },
     input: {
       height: 40,
       width: '100%',
       borderColor: 'gray',
       borderWidth: 1,
-      marginBottom: '5%',
-      marginTop: '3%',
-      borderRadius:15,
+      marginBottom: 10, // Altere este valor conforme necessário
+      marginTop: 10, // Altere este valor conforme necessário
+      borderRadius: 15,
+      paddingLeft: 10, // Adiciona espaço à esquerda do texto
+      paddingRight: 10
     },
     createForgotLink: {
       flexDirection: 'row',
       alignSelf: 'flex-end',
       marginBottom: '5%',
-      
-      
     },
     link: {
       color : '#2196F3',
-      marginLeft: '10%'
+      marginLeft: '10%',
+  
     },
-    
     logo: {
       alignItems: 'center',
       marginBottom: '10%'
     },
     text:{
       fontSize: 34,
-      color: 'gray',
+      color: '#999999',
+    
     }
   });
   export default styles
