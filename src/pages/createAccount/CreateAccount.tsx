ed292deb5825a9d84756e0a6594b3eb333896b6f
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-11-20 21:27:33 -0300

    Update CreateAccount.tsx

--- src/pages/createAccount/CreateAccount.tsx
+++ src/pages/createAccount/CreateAccount.tsx
@@ -13,7 +13,7 @@ const CreateAccount = ({navigation}:any) => {
     <View style={styles.container}>
       <View style={styles.logo}>
       <Icon name = 'adduser' size={150} color={'#2196F3'}></Icon>
-        <Text style={styles.text}>PharmGO </Text>
+        <Text style={styles.text}>Criar Usuário </Text>
       </View>
       <Text>Nome Completo: </Text>
       <TextInput style = {styles.input}/>
