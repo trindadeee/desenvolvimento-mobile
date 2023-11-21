89020e5cc8d034c422f9de5463bf7510efa6f6cf
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-11-20 21:02:21 -0300

    Update Login.tsx

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,30 +1,34 @@
 import React from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import { Button, ImageBackground, Text, TextInput, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './LoginStyle';
-
 
 const Login = ({navigation}:any) => {
     const goToPage = (path:string) => {
         navigation.navigate(path)
     }
   return (
-    <View style={styles.container}>
+
+    <ImageBackground style={styles.imageBack} source={require('pharmacy-mobile/assets/farmBackground.jpg')}>
+   
+     <View style={styles.container} >
       <View style = {styles.logo} >
         <Icon name = 'plus-square' size={150} color={'#2196F3'}></Icon>
-        <Text style = {styles.text}> PharmGO</Text>
+        <Text style = {styles.text}> ...</Text>
       </View>
-      <Text>Login:</Text>
+      <Text style={{fontWeight: 'bold'}}>Login:</Text>
       <TextInput style = {styles.input}/>
-      <Text>Senha:</Text>
+      <Text style={{fontWeight: 'bold'}}>Senha:</Text>
       <TextInput secureTextEntry={true} style = {styles.input}/>
       <View style = {styles.createForgotLink}>
-        <Text onPress={() => {goToPage('createAccount')}} style = {styles.link}>Criar Usuário</Text>
-        <Text onPress={() => {goToPage('forgotPassword')}} style = {styles.link}>Esqueceu sua senha?</Text>
+        <Text onPress={() => {goToPage('createAccount')}} style = {[styles.link,{fontWeight: 'bold'}]}>Criar Usuário</Text>
+        <Text onPress={() => {goToPage('forgotPassword')}} style = {[styles.link,{fontWeight: 'bold'}]}>Esqueceu sua senha?</Text>
       </View>
       <Button onPress={() => {goToPage('home')}} title = 'Entrar'></Button>
     </View>
+        
+    </ImageBackground>
   )
-}
+};
 
-export default Login
+export default Login;
