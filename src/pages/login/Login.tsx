e1e54d3e2655a0cd6347258b1f31c734de218fd9
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-11-24 13:02:03 -0300

    Update Login.tsx

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, ImageBackground, Pressable, Text, TextInput, View } from 'react-native';
+import { ImageBackground, Pressable, Text, TextInput, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './LoginStyle';
 
