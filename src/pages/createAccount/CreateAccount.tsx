88bf1c9bd0037217b2dc0966c411dfa4592fbe75
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-11-25 15:43:19 -0300

    Update CreateAccount.tsx

--- src/pages/createAccount/CreateAccount.tsx
+++ src/pages/createAccount/CreateAccount.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import {Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { ImageBackground, Pressable, Text, TextInput, View } from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
-import styles from './CreateAccountStyle';
+import styles from '../login/LoginStyle';
 
 
 const CreateAccount = ({navigation}:any) => {
+  const backgroundImageUrl = 'https://img.freepik.com/vetores-premium/molecula-de-pesquisa-de-dna-de-formacao-medica-abstrata_230610-1390.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais';
   const goToPage = (path:String) => {
     navigation.navigate(path)
   }
   return (
-    
+    <ImageBackground
+        source={{ uri: backgroundImageUrl }}
+        style={styles.imageBack}
+      >
     <View style={styles.container}>
       <View style={styles.logo}>
       <Icon name = 'adduser' size={150} color={'#2196F3'}></Icon>
@@ -25,17 +29,27 @@ const CreateAccount = ({navigation}:any) => {
       <TextInput secureTextEntry={true} style={styles.input}/>
       <View style={styles.createForgotLink}>
       </View>
-
-      <TouchableOpacity style={styles.customButton} onPress={() => { goToPage('login') }}>
-          <Text style={styles.buttonText}>Create</Text>
-        </TouchableOpacity>
-     
+      <Pressable
+               
+                style={({ pressed }: any) => ({
+                  backgroundColor: pressed ? '#95CEDF' : '#236B8E',
+                  height: 40,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 15,
+                  marginBottom: 10,
+                })}
+              >
+                <Text style={{ fontSize: 18, color: '#FFFFFF' }}>Enviar</Text>
+              </Pressable>
     </View>
-   
+    </ImageBackground>
   );
 };
 
 export default CreateAccount;
+
+
 
 
 
