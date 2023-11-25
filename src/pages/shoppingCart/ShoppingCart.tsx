cda02c697e144f570f26563fea6eabbebe07eae9
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-11-25 15:38:53 -0300

    Update ShoppingCart.tsx

--- src/pages/shoppingCart/ShoppingCart.tsx
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -3,46 +3,93 @@ import { ImageBackground, Text, View, Image, Pressable } from 'react-native';
 import styles from '../login/LoginStyle';
 
 const ShoppingCart = ({ route, navigation }: any) => {
-  const { shoppingCart } = route.params;
+  const backgroundImageUrl = 'https://img.freepik.com/vetores-premium/molecula-de-pesquisa-de-dna-de-formacao-medica-abstrata_230610-1390.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais';
 
-  const removeItem = (index: number) => {
-    const updatedCart = [...shoppingCart];
-    const itemToRemove = updatedCart[index];
+  const [cart, setCart] = useState(route.params.shoppingCart);
 
-    if (itemToRemove.quantity > 1) {
-      itemToRemove.quantity -= 1;
-    } else {
-      updatedCart.splice(index, 1);
+  const updateQuantity = (id: number, increment: number) => {
+    const updatedCart = [...cart];
+    const index = updatedCart.findIndex((item) => item.id === id);
+
+    if (index !== -1) {
+      updatedCart[index].quantity += increment;
+
+      if (updatedCart[index].quantity <= 1) {
+        updatedCart.splice(index, 1);
+      }
+
+      setCart(updatedCart);
+      navigation.setParams({ shoppingCart: updatedCart });
     }
+  };
+
+  const removeItem = (id: number) => {
+    const updatedCart = cart.filter((item:any) => item.id !== id);
+    setCart(updatedCart);
     navigation.setParams({ shoppingCart: updatedCart });
   };
 
   return (
-    <ImageBackground style={styles.imageBack} source={require('pharmacy-mobile/assets/farmBackground.jpg')}>
       <>
-        {shoppingCart.map((prod: any, i: number) => (
-          <View key={i} style={{ display: 'flex', alignItems: 'center', marginTop: 10, marginBottom: 10 }}>
+      <ImageBackground
+        source={{ uri: backgroundImageUrl }}
+        style={styles.imageBack}
+      >
+        {cart.map((prod: any) => (
+          <View key={prod.id} style={{ display: 'flex', alignItems: 'center', flexDirection: 'row', marginTop: 10, marginBottom: 10 }}>
             <Image source={{ uri: prod.image }} style={{ width: 100, height: 100 }} />
-            <Text>{prod.name}</Text>
-            <Text>Quantidade: {prod.quantity}</Text>
-            <View style={{ margin: 10, width: '95%' }}>
-            <Pressable
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 10, marginRight: 'auto' }}>
+              <Pressable
                 style={({ pressed }: any) => ({
                   backgroundColor: pressed ? '#95CEDF' : '#236B8E',
                   height: 40,
+                  width: 50,
                   justifyContent: 'center',
                   alignItems: 'center',
-                  borderRadius: 15,
-                  marginBottom: 10,
+                  borderRadius: 18,
+                  marginRight: 8,
                 })}
+                onPress={() => updateQuantity(prod.id, -1)}
               >
-                <Text style={{ fontSize: 18, color: '#FFFFFF' }}>Remover</Text>
+                <Text style={{ fontSize: 18, color: '#FFFFFF' }}>-</Text>
+              </Pressable>
+              <View>
+                <Text>{prod.name}</Text>
+                <Text>Quantidade: {prod.quantity}</Text>
+              </View>
+              <Pressable
+                style={({ pressed }: any) => ({
+                  backgroundColor: pressed ? '#95CEDF' : '#236B8E',
+                  height: 40,
+                  width: 50,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 18,
+                  marginLeft: 8,
+                })}
+                onPress={() => updateQuantity(prod.id, 1)}
+              >
+                <Text style={{ fontSize: 18, color: '#FFFFFF' }}>+</Text>
               </Pressable>
             </View>
+            <Pressable
+              style={({ pressed }: any) => ({
+                backgroundColor: pressed ? '#95CEDF' : '#236B8E',
+                height: 40,
+                width: 140,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: 15,
+                marginRight: 20, // Ajuste a margem à esquerda para afastar da borda
+              })}
+              onPress={() => removeItem(prod.id)}
+            >
+              <Text style={{ fontSize: 18, color: '#FFFFFF' }}>Remover</Text>
+            </Pressable>
           </View>
         ))}
+        </ImageBackground>
       </>
-    </ImageBackground>
   );
 };
 
