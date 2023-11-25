1ef21ce59d052c6d19c495eec033076b4ec8c13d
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-11-25 15:42:14 -0300

    Update ForgotPassword.tsx

--- src/pages/forgotPassword/ForgotPassword.tsx
+++ src/pages/forgotPassword/ForgotPassword.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import { ImageBackground, Pressable, Text, TextInput, View } from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
 import styles from '../login/LoginStyle';
 
 const ForgotPassword = ({navigation}:any) => {
+  const backgroundImageUrl = 'https://img.freepik.com/vetores-premium/molecula-de-pesquisa-de-dna-de-formacao-medica-abstrata_230610-1390.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais';
+
   const goToPage = (path:String) => {
     navigation.navigate(path)
   }
   return (
+    <ImageBackground
+        source={{ uri: backgroundImageUrl }}
+        style={styles.imageBack}
+      >
     <View style={styles.container}>
     <View style = {styles.logo} >
       <Icon name = 'key' size={150} color={'#2196F3'}></Icon>
@@ -19,8 +25,21 @@ const ForgotPassword = ({navigation}:any) => {
     <TextInput secureTextEntry={true} style = {styles.input}/>
     <Text>Confirmar Nova Senha:</Text>
     <TextInput secureTextEntry={true} style = {styles.input}/>
-    <Button onPress={() => {goToPage('login')}} title = 'Enviar' ></Button>
+    <Pressable
+               
+                style={({ pressed }: any) => ({
+                  backgroundColor: pressed ? '#95CEDF' : '#236B8E',
+                  height: 40,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 15,
+                  marginBottom: 10,
+                })}
+              >
+                <Text style={{ fontSize: 18, color: '#FFFFFF' }}>Enviar</Text>
+              </Pressable>
   </View>
+  </ImageBackground>
   )
 }
 
