0b9c40138125a89f87a5c9be8012b0c20a176a29
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-11-26 02:11:37 -0300

    Update ForgotPassword.tsx

--- src/pages/forgotPassword/ForgotPassword.tsx
+++ src/pages/forgotPassword/ForgotPassword.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ImageBackground, Pressable, Text, TextInput, View } from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
 import styles from '../login/LoginStyle';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const ForgotPassword = ({navigation}:any) => {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [confirmationMessage, setConfirmationMessage] = useState('');
+
   const backgroundImageUrl = 'https://img.freepik.com/vetores-premium/molecula-de-pesquisa-de-dna-de-formacao-medica-abstrata_230610-1390.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais';
 
   const goToPage = (path:String) => {
     navigation.navigate(path)
   }
+  const handleCreateAccount = () => {
+    setConfirmationMessage('Senha Alterada com Sucesso!');
+    setModalVisible(true);
+  };
   return (
     <ImageBackground
         source={{ uri: backgroundImageUrl }}
@@ -16,24 +24,27 @@ const ForgotPassword = ({navigation}:any) => {
       >
     <View style={styles.container}>
     <View style = {styles.logo} >
-      <Icon name = 'key' size={150} color={'#2196F3'}></Icon>
+      <MaterialCommunityIcons name = 'shield-key-outline' size={150} color={'#236B8E'}></MaterialCommunityIcons>
       <Text style = {styles.text}> Recuperar Senha</Text>
     </View>
-    <Text>Email:</Text>
+    <Text style={{fontSize:15,color:'#236B8E', fontWeight: 'bold'}}>Email:</Text>
     <TextInput style = {styles.input}/>
-    <Text>Nova Senha:</Text>
+    <Text style={{fontSize:15,color:'#236B8E', fontWeight: 'bold'}}>Nova Senha:</Text>
     <TextInput secureTextEntry={true} style = {styles.input}/>
-    <Text>Confirmar Nova Senha:</Text>
+    <Text style={{fontSize:15,color:'#236B8E', fontWeight: 'bold'}}>Confirmar Nova Senha:</Text>
     <TextInput secureTextEntry={true} style = {styles.input}/>
     <Pressable
                
                 style={({ pressed }: any) => ({
                   backgroundColor: pressed ? '#95CEDF' : '#236B8E',
                   height: 40,
+                  width:'60%',
+                  alignSelf:'center',
                   justifyContent: 'center',
                   alignItems: 'center',
                   borderRadius: 15,
                   marginBottom: 10,
+                  marginTop: 30
                 })}
               >
                 <Text style={{ fontSize: 18, color: '#FFFFFF' }}>Enviar</Text>
