716de9ba5411953975082b4da926c966397a327c
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-11-26 16:25:13 -0300

    Add files via upload

---
+++ src/favorites/FavoritesStyle.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  imageBack: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  container: {
+    flexGrow: 1,
+  },
+  cardsContainer: {
+    flexDirection: 'row', // Alinha os cards lado a lado
+    flexWrap: 'wrap', // Permite que os cards quebrem para a linha seguinte, se necessário
+    justifyContent: 'space-around', // Espaçamento igual entre os cards
+    padding: 10,
+  },
+  cardContainer: {
+    backgroundColor: 'transparent',
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 150,
+  },
+});
+
+export default styles;
