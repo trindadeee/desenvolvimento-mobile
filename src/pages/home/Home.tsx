828448cd1038a4a6117cb6ca66bebec051284940
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-11-26 16:02:10 -0300

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import { ScrollView, View, Pressable, TextInput, ToastAndroid, ImageBackground } from 'react-native';
 import { Card, Text } from 'react-native-elements';
-import { StatusBar } from 'expo-status-bar';
+import { useNavigation } from '@react-navigation/native'; // Importe o hook useNavigation
 import styles from '../login/LoginStyle';
 import { products } from '../api/product';
 import Icon from 'react-native-vector-icons/AntDesign';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { StatusBar } from 'react-native';
 
 const Home = ({ shoppingCart, setShoppingCart, favorites, setFavorites }: any) => {
   const backgroundImageUrl = 'https://img.freepik.com/vetores-premium/molecula-de-pesquisa-de-dna-de-formacao-medica-abstrata_230610-1390.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais';
 
   const [searchTerm, setSearchTerm] = useState('');
   const [showFavoritesOnly, setShowFavoritesOnly] = useState(false);
+
+  const navigation = useNavigation(); // Use o hook useNavigation para obter o objeto de navegação
 
   const openToast = (message: string) => {
     ToastAndroid.show(message, 3000);
@@ -40,17 +43,18 @@ const Home = ({ shoppingCart, setShoppingCart, favorites, setFavorites }: any) =
     if (isFavorite) {
       removeFavorite(product);
     } else {
-      setFavorites([...favorites, product]);
+      // Adicione o produto aos favoritos
+      setFavorites([...favorites, { ...product }]);
     }
   };
 
   return (
     <ImageBackground
-        source={{ uri: backgroundImageUrl }}
-        style={styles.imageBack}
-      >
+      source={{ uri: backgroundImageUrl }}
+      style={styles.imageBack}
+    >
       <ScrollView>
-        <StatusBar backgroundColor='gray' />
+        <StatusBar backgroundColor="#236B8E" />
 
         <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
           <TextInput
@@ -70,14 +74,18 @@ const Home = ({ shoppingCart, setShoppingCart, favorites, setFavorites }: any) =
               fontSize: 17
             }}
           />
-
           <Pressable
-            onPress={() => setShowFavoritesOnly(!showFavoritesOnly)}
-            style={({ pressed }: any) => ({
+            onPress={() => {
+              setShowFavoritesOnly(!showFavoritesOnly);
+              // Navegue para a página de favoritos ao pressionar o botão
+              navigation.navigate('favorites', { favorites })
+            }}
+            style={({ pressed }) => ({
               padding: 10,
+              opacity: pressed ? 0.5 : 1,
             })}
           >
-            <Icon name="heart" size={28} color={showFavoritesOnly ? 'red' : '#236B8E'} />
+            <Icon name="heart" size={28} color="#236B8E" />
           </Pressable>
         </View>
 
@@ -85,46 +93,38 @@ const Home = ({ shoppingCart, setShoppingCart, favorites, setFavorites }: any) =
           {products.map((product, i) => {
             const isFavorite = favorites.some((fav: any) => fav.name === product.name);
 
-            if (
-              (showFavoritesOnly && isFavorite) ||
-              product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-              product.price.toLowerCase().includes(searchTerm.toLowerCase())
-            ) {
-              return (
-                <Card key={i} containerStyle={{ backgroundColor: 'transparent' }}>
-                  <Card.Image source={{ uri: product.image }} />
-                  <Text>{product.name}</Text>
-                  <Text>{product.price}</Text>
-                  <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-                    <Pressable 
-                        onPress={() => {
-                        openToast('Item Adicionado com Sucesso!');
-                        addItemToCart([...shoppingCart], product);
-                        }}>
-                      <FontAwesome name="cart-plus" size={28} color="#236B8E" />
-                      </Pressable>
-                      <Pressable onPress={() => toggleFavorite(product)}>
-                        <Icon 
-                          onPress={() => {
-                          toggleFavorite(product);
-                          }}
-                          name={isFavorite ? 'heart' : 'hearto'}
-                          size={28}
-                          color={isFavorite ? 'red' : '#236B8E'}
-                          style={{marginBottom: 20, marginLeft: 10}}
-                          /> 
-                      </Pressable>
-                    </View>
-                  </Card>
-                  );
-                } else {
-                return null;
-              }
-              })}
-          </View>
-        </ScrollView>
-        </ImageBackground>
-    );
+            return (
+              <Card key={i} containerStyle={{ backgroundColor: 'transparent' }}>
+                <Card.Image source={{ uri: product.image }} />
+                <Text>{product.name}</Text>
+                <Text>{product.price}</Text>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+                  <Pressable
+                    onPress={() => {
+                      openToast('Item Adicionado com Sucesso!');
+                      addItemToCart([...shoppingCart], product);
+                    }}
+                  >
+                    <FontAwesome name="cart-plus" size={28} color="#236B8E" />
+                  </Pressable>
+                  <Pressable onPress={() => toggleFavorite(product)}>
+                    <Icon
+                      onPress={() => toggleFavorite(product)}
+                      name={isFavorite ? 'heart' : 'hearto'}
+                      size={28}
+                      color={isFavorite ? 'red' : '#236B8E'}
+                      style={{ marginBottom: 20, marginLeft: 10 }}
+                    />
+                  </Pressable>
+                </View>
+              </Card>
+            );
+          })}
+        </View>
+      </ScrollView>
+    </ImageBackground>
+  );
 };
 
 export default Home;
+
