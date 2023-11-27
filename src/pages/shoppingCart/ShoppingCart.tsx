3b326982734583362dd1ef6bcd4e6bf85475a72b
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-11-27 13:52:24 -0300

    Update ShoppingCart.tsx

--- src/pages/shoppingCart/ShoppingCart.tsx
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -1,103 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import { ImageBackground, Text, View, Image, Pressable } from 'react-native';
+import React, { useState } from 'react';
+import { ImageBackground, Pressable, Text, TextInput, View, Linking } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'; // Para ícone do Instagram
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons'; // Para ícone do Twitter
 import styles from '../login/LoginStyle';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { StatusBar } from 'expo-status-bar';
 
-const ShoppingCart = ({ route, navigation }: any) => {
-  const backgroundImageUrl = 'https://img.freepik.com/vetores-premium/molecula-de-pesquisa-de-dna-de-formacao-medica-abstrata_230610-1390.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais';
 
-  const [cart, setCart] = useState(route.params.shoppingCart);
+const CreateAccount = ({ navigation }: any) => {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [confirmationMessage, setConfirmationMessage] = useState('');
 
-  useEffect(() => {
-    // Atualizar o carrinho quando a propriedade de navegação mudar
-    setCart(route.params.shoppingCart);
-  }, [route.params.shoppingCart]);
+  const backgroundImageUrl ='https://img.freepik.com/vetores-premium/molecula-de-pesquisa-de-dna-de-formacao-medica-abstrata_230610-1390.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais';
 
-  const updateQuantity = (id: number, increment: number) => {
-    const updatedCart = [...cart];
-    const index = updatedCart.findIndex((item) => item.id === id);
-
-    if (index !== -1) {
-      updatedCart[index].quantity += increment;
-
-      if (updatedCart[index].quantity <= 0) {
-        updatedCart.splice(index, 1);
-      }
-
-      setCart(updatedCart);
-      navigation.setParams({ shoppingCart: updatedCart });
-    }
+  const goToPage = (path: string) => {
+    navigation.navigate(path);
   };
 
-  const removeItem = (id: number): void => {
-    const updatedCart = cart.filter((item: { id: number }) => item.id !== id);
-    setCart(updatedCart);
-    navigation.setParams({ shoppingCart: updatedCart });
+  const handleCreateAccount = () => {
+    setConfirmationMessage('Conta criada com sucesso!');
+    setModalVisible(true);
   };
 
   return (
-    <>
-      <ImageBackground
-        source={{ uri: backgroundImageUrl }}
-        style={styles.imageBack}
-      >
-        {cart.map((prod: any) => (
-          prod.quantity > 0 && (
-            <View key={prod.id} style={{ display: 'flex', alignItems: 'center', flexDirection: 'row', marginTop: 10, marginBottom: 10 ,backgroundColor: 'white'}}>
-              <Image source={{ uri: prod.image }} style={{ width: 100, height: 100 }} />
-              <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 10, flex: 1 }}>
-                <Pressable
-                  style={({ pressed }) => ({
-                    backgroundColor: pressed ? '#95CEDF' : '#236B8E',
-                    height: 40,
-                    width: 50,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: 18,
-                    marginRight: 8,
-                  })}
-                  onPress={() => updateQuantity(prod.id, -1)}
-                >
-                  <Text style={{ fontSize: 18, color: '#FFFFFF' }}>-</Text>
-                </Pressable>
-                <View>
-                  <Text style={{ fontSize: 15, color: '#236B8E', fontWeight: 'bold' }}>{prod.name}</Text>
-                  <Text style={{ fontSize: 15, color: '#236B8E', fontWeight: 'bold' }}>Quantidade: {prod.quantity}</Text>
-                </View>
-                <Pressable
-                  style={({ pressed }) => ({
-                    backgroundColor: pressed ? '#95CEDF' : '#236B8E',
-                    height: 40,
-                    width: 50,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: 18,
-                    marginLeft: 8,
-                  })}
-                  onPress={() => updateQuantity(prod.id, 1)}
-                >
-                  <Text style={{ fontSize: 18, color: '#FFFFFF' }}>+</Text>
-                </Pressable>
-              </View>
-              <Pressable
-                style={({ pressed }) => ({
-                  backgroundColor: pressed ? '#95CEDF' :'#236B8E',
-                  height: 40,
-                  width: 140,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  borderRadius: 15,
-                  marginRight: 20,
-                })}
-                onPress={() => removeItem(prod.id)}
-              >
-                <Text style={{ fontSize: 18, color: '#FFFFFF' }}>Remover</Text>
-              </Pressable>
-            </View>
-          )
-        ))}
-      </ImageBackground>
-    </>
+    <ImageBackground source={{ uri: backgroundImageUrl }} style={styles.imageBack}>
+      <StatusBar backgroundColor='#236B8E' />
+
+      <View style={styles.container}>
+        <View style={styles.logo}>
+          <FontAwesome name="user-circle-o" size={150} color={'#236B8E'}></FontAwesome>
+          <Text style={styles.text}>Criar Usuário </Text>
+        </View>
+        <Text style={{fontSize:15,color:'#236B8E', fontWeight: 'bold'}}>Nome Completo: </Text>
+        <TextInput style={styles.input} />
+        <Text style={{fontSize:15,color:'#236B8E', fontWeight: 'bold'}}>Email: </Text>
+        <TextInput secureTextEntry={true} style={styles.input} />
+        <Text style={{fontSize:15,color:'#236B8E', fontWeight: 'bold'}}>Senha: </Text>
+        <TextInput secureTextEntry={true} style={styles.input} />
+        <Text style={{fontSize:15,color:'#236B8E', fontWeight: 'bold'}}>Confirmar Senha: </Text>
+        <TextInput secureTextEntry={true} style={styles.input} />
+
+        <View style={styles.createForgotLink}>
+
+        <Pressable
+          style={({ pressed }: any) => ({
+            backgroundColor: pressed ? '#95CEDF' : '#236B8E',
+            height: 40,
+            width: '60%',
+            justifyContent: 'center',
+            alignSelf:'center',
+            alignItems:'center',
+            borderRadius: 15,
+            marginBottom: 20,
+            marginTop: 50,
+            marginRight:112
+          })}
+        >
+          <Text style={{ fontSize: 18, color: '#FFFFFF' }}>Enviar</Text>
+        </Pressable>
+        </View>
+      </View>
+    </ImageBackground>
   );
 };
 
-export default ShoppingCart;
+export default CreateAccount;
+
