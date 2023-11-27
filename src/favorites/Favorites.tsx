36398f950f7564d85cfd6251c5efd243aacb21b6
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-11-26 23:52:34 -0300

    Update Favorites.tsx

--- src/favorites/Favorites.tsx
+++ src/favorites/Favorites.tsx
@@ -1,12 +1,17 @@
-// Favorites.tsx
 import React from 'react';
-import { View, ScrollView, Text, ImageBackground } from 'react-native';
+import { View, ScrollView, Text, ImageBackground, TouchableOpacity } from 'react-native';
 import { Card, Image } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/AntDesign';
 import styles from './FavoritesStyle';
 
-const Favorites = ({ route }: any) => {
+const Favorites = ({ route, navigation }: any) => {
   const { favorites = [] } = route?.params || {};
   const backgroundImageUrl = 'https://img.freepik.com/vetores-premium/molecula-de-pesquisa-de-dna-de-formacao-medica-abstrata_230610-1390.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais';
+
+  const removeFavorite = (id: number) => {
+    const updatedFavorites = (favorites || []).filter((item: { id: number }) => item.id !== id);
+    navigation.setParams({ favorites: updatedFavorites });
+  };
 
   return (
     <ImageBackground
@@ -17,12 +22,15 @@ const Favorites = ({ route }: any) => {
         <View style={styles.cardsContainer}>
           {favorites.map((favorite: any, index: any) => (
             <Card key={index} containerStyle={styles.cardContainer}>
+              <TouchableOpacity onPress={() => removeFavorite(favorite.id)}>
+                <Icon name="heart" size={24} color="#ff0000" />
+              </TouchableOpacity>
               <Image
                 source={{ uri: favorite.image }}
                 style={styles.image}
               />
-              <Text  style={{fontSize:14,color:'#236B8E', fontWeight: 'bold'}}>{favorite.name}</Text>
-              <Text  style={{fontSize:14,color:'#236B8E', fontWeight: 'bold'}}>{favorite.price}</Text>
+              <Text style={{ fontSize: 14, color: '#236B8E', fontWeight: 'bold' }}>{favorite.name}</Text>
+              <Text style={{ fontSize: 14, color: '#236B8E', fontWeight: 'bold' }}>{favorite.price}</Text>
             </Card>
           ))}
         </View>
