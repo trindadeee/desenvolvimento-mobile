5144f7c741c06206b113029d2568bf6162bf4524
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-12-06 16:01:42 -0300

    Update ShoppingCart.tsx
    acrescentou total, botao de finalizar compras e redirecionamento para pagina de pedidos

--- src/pages/shoppingCart/ShoppingCart.tsx
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -3,14 +3,23 @@ import { ImageBackground, Text, View, Image, Pressable } from 'react-native';
 import styles from './ShoppingStyle';
 
 const ShoppingCart = ({ route, navigation }: any) => {
-  const backgroundImageUrl = 'https://img.freepik.com/vetores-premium/molecula-de-pesquisa-de-dna-de-formacao-medica-abstrata_230610-1390.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais';
+  const backgroundImageUrl =
+    'https://img.freepik.com/vetores-premium/molecula-de-pesquisa-de-dna-de-formacao-medica-abstrata_230610-1390.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais';
 
   const [cart, setCart] = useState(route.params.shoppingCart);
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
-    // Atualizar o carrinho quando a propriedade de navegação mudar
+    
     setCart(route.params.shoppingCart);
+    updateTotal(route.params.shoppingCart);
   }, [route.params.shoppingCart]);
+
+  const updateTotal = (updatedCart: any[]) => {
+
+    const newTotal = updatedCart.reduce((acc, prod) => acc + prod.quantity * prod.price, 0);
+    setTotal(newTotal);
+  };
 
   const updateQuantity = (id: number, increment: number) => {
     const updatedCart = [...cart];
@@ -24,21 +33,31 @@ const ShoppingCart = ({ route, navigation }: any) => {
       }
 
       setCart(updatedCart);
+      updateTotal(updatedCart);
       navigation.setParams({ shoppingCart: updatedCart });
     }
   };
 
   return (
     <>
-      <ImageBackground
-        source={{ uri: backgroundImageUrl }}
-        style={styles.imageBack}
-      >
+      <ImageBackground source={{ uri: backgroundImageUrl }} style={styles.imageBack}>
         {cart.map((prod: any) => (
           prod.quantity > 0 && (
-            <View key={prod._id} style={{ alignItems: 'center', marginVertical: 10, backgroundColor: 'white', marginHorizontal: 6, borderRadius: 15, padding: 10 }}>
+            <View
+              key={prod._id}
+              style={{
+                alignItems: 'center',
+                marginVertical: 10,
+                backgroundColor: 'white',
+                marginHorizontal: 6,
+                borderRadius: 15,
+                padding: 10,
+              }}
+            >
               <Image source={{ uri: prod.image }} style={{ width: 100, height: 100 }} />
-              <Text style={{ fontSize: 15, color: '#236B8E', fontWeight: 'bold', marginTop: 10 }}>{prod.name}</Text>
+              <Text style={{ fontSize: 15, color: '#236B8E', fontWeight: 'bold', marginTop: 10 }}>
+                {prod.name}
+              </Text>
               <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                 <Pressable
                   style={({ pressed }) => ({
@@ -54,7 +73,9 @@ const ShoppingCart = ({ route, navigation }: any) => {
                 >
                   <Text style={{ fontSize: 18, color: '#FFFFFF' }}>-</Text>
                 </Pressable>
-                <Text style={{ fontSize: 15, color: '#236B8E', fontWeight: 'bold' }}>Quantidade: {prod.quantity}</Text>
+                <Text style={{ fontSize: 15, color: '#236B8E', fontWeight: 'bold' }}>
+                  Quantidade: {prod.quantity}
+                </Text>
                 <Pressable
                   style={({ pressed }) => ({
                     backgroundColor: pressed ? '#95CEDF' : '#236B8E',
@@ -74,6 +95,28 @@ const ShoppingCart = ({ route, navigation }: any) => {
           )
         ))}
       </ImageBackground>
+      <View style={styles.totalContainer}>
+        <Text style={[styles.totalText, {textAlign:'center'}]}>Total: R$ {total.toFixed(2)}</Text>
+        </View>
+        <View style={{alignSelf:'center',marginBottom: 27}}>
+        <Pressable
+          style={({ pressed }) => ({
+            backgroundColor: pressed ? '#95CEDF' : '#236B8E',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 20,
+            marginTop: 10,
+            height: 40,
+            width: 200,
+          
+          })}
+          onPress={() => {
+            navigation.navigate('order', { shoppingCart: cart, total });
+          }}
+        >
+          <Text style={{ fontSize: 18, color: '#FFFFFF' }}>Finalizar Compra</Text>
+        </Pressable>
+        </View>
     </>
   );
 };
