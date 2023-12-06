e8820919c6de6849398c29cdc2661ad6156ce029
Luanna Fernandes Flor de Souza <141292139+Luanna88@users.noreply.github.com>
2023-12-06 00:17:53 -0300

    Update Favorites.tsx

--- src/pages/favorites/Favorites.tsx
+++ src/pages/favorites/Favorites.tsx
@@ -4,15 +4,12 @@ import { Card, Image } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/AntDesign';
 import styles from './FavoritesStyle';
 
-const Favorites = ({ route, navigation }: any) => {
-  const { favorites = [] } = route?.params || {};
+const Favorites = ({ favorites, setFavorites }: any) => {
   const backgroundImageUrl = 'https://img.freepik.com/vetores-premium/molecula-de-pesquisa-de-dna-de-formacao-medica-abstrata_230610-1390.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais';
 
   const removeFavorite = (id: number) => {
-    console.log(id)
-    //Não funcionando
     const updatedFavorites = (favorites || []).filter((item: { id: number }) => item.id !== id);
-    navigation.setParams({ favorites: updatedFavorites });
+    setFavorites(updatedFavorites)
   };
 
   return (
@@ -24,17 +21,15 @@ const Favorites = ({ route, navigation }: any) => {
         <View style={styles.cardsContainer}>
           {favorites.map((favorite: any, index: any) => (
             <Card key={index} containerStyle={styles.cardContainer}>
-              <TouchableOpacity onPress={() => removeFavorite(favorite._id)}>
+              <TouchableOpacity onPress={() => removeFavorite(favorite.id)}>
+                <Icon name="heart" size={24} color="#ff0000" />
               </TouchableOpacity>
               <Image
                 source={{ uri: favorite.image }}
                 style={styles.image}
               />
-              <Text style={{ fontSize: 14, color: '#236B8E', fontWeight: 'bold', paddingVertical: 2}}>{favorite.name}</Text>
-              <Text style={{ fontSize: 14, color: '#236B8E', fontWeight: 'bold', paddingVertical: 2}}>R${favorite.price}.00</Text>
-              <TouchableOpacity onPress={() => removeFavorite(favorite._id)}>
-                <Icon name="heart" size={27} color="#ff0000" />
-              </TouchableOpacity>
+              <Text style={{ fontSize: 14, color: '#236B8E', fontWeight: 'bold' }}>{favorite.name}</Text>
+              <Text style={{ fontSize: 14, color: '#236B8E', fontWeight: 'bold' }}>{favorite.price}</Text>
             </Card>
           ))}
         </View>
@@ -44,3 +39,5 @@ const Favorites = ({ route, navigation }: any) => {
 };
 
 export default Favorites;
+
+
